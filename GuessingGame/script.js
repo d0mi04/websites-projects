b8e084c862ff17c2ffd12b812338c633474b54ce
Dominika Windows <d0mi04@windows.com>
2025-04-11 17:01:52 +0200

    creating GuessingGame

---
+++ GuessingGame/script.js
@@ -0,0 +1,32 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 10;
+
+function checkGuess() {
+  attempts--;
+
+  while(attempts > 0) {
+    let inputElement = document.getElementById('guess');
+    let feedbackElement = document.getElementById('feedback');
+
+    let guess = inputElement.value;
+    if(guess === randomNumber) {
+      feedbackElement.innerHTML = "Success!";
+      feedbackElement.style.color = "green";
+      attempts = 0;
+      break;
+    } else if(guess < randomNumber) {
+      feedbackElement.innerHTML = "Too low! Try again.";
+      feedbackElement.style.color = "red";
+      break;
+    } else {
+      feedbackElement.innerHTML = "Too high! Try again.";
+      feedbackElement.style.color = "red";
+      break;
+    }
+  }
+
+  if(attempts == 0 && guess !== randomNumber) {
+    feedbackElement.innerHTML = "Game over";
+  }
+}
+
