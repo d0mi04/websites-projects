c847f515afb742f0e0310fea65b88a829ed89df2
Dominika Windows <d0mi04@windows.com>
2025-04-16 15:11:51 +0200

    update Rick and Morty characters page

--- Rick-and-Morty-character-page/script.js
+++ Rick-and-Morty-character-page/script.js
@@ -1,4 +1,7 @@
 let charactersContainer = document.getElementById("characters-container");
+const characterInfoModal = document.getElementById("character-info");
+const closeBtn = document.getElementById("close");
+const characterDetailContent = document.getElementById("character-detail-content");
 fetch('https://rickandmortyapi.com/api/character')
   .then(response => response.json())
   .then(data => {
@@ -12,5 +15,28 @@ fetch('https://rickandmortyapi.com/api/character')
     <p class="charcter-detail"><span class="attribute">Status:</span> ${character.status}</p>
     <p class="character-detail"><span class="attribute">Spieces:</span> ${character.species}</p>
   `;
+
+    const img = card.querySelector(".character-image");
+    img.addEventListener("click", () => {
+      characterDetailContent.innerHTML = `
+        <img src="${character.image}" alt="${character.name}" class="character-image">
+        <p><span class="attribute">Name:</span> ${character.name}</p>
+        <p><span class="attribute">Status:</span> ${character.status}</p>
+        <p><span class="attribute">Species:</span> ${character.species}</p>
+        <p><span class="attribute">Gender:</span> ${character.gender}</p>
+        <p><span class="attribute">Origin:</span> ${character.origin.name}</p>
+      `;
+      characterInfoModal.style.display = "block";
     });
+    });
+  });
+
+  closeBtn.addEventListener("click", () => {
+    characterInfoModal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if(event.target === characterInfoModal) {
+      characterInfoModal.style.display = "none";
+    }
   });
