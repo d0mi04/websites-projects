a2db4a51193780731073ab3f3b2a55c485e10f00
Dominika Windows <d0mi04@windows.com>
2025-04-18 21:57:21 +0200

    planet quiz game - part2

--- Little-planet-quiz/script.js
+++ Little-planet-quiz/script.js
@@ -1,24 +1,41 @@
-let question = "What planet is known as the 'Red Planet'?";
-let choices = ["Earth", "Mars", "Jupiter", "Saturn"];
-const correctAnswer = "Mars";
+let score = 0;
+let questions = [
+  "What planet is known as the 'Red Planet'?",
+  "What is the biggest planet in Solar System?"
+];
+let currentQuestionIndex = 0;
+
+let choicesArray = [
+  ["Earth", "Mars", "Jupiter", "Saturn"],
+  ["Mercury", "Venus", "Jupiter", "Neptun"]
+];
+let correctAnswers = ["Mars", "Jupiter"];
 
 function checkAnswer(button) {
-  if(button.value === correctAnswer) {
-    document.getElementById("result").innerHTML = "Correct!";
-  } else {
-    document.getElementById("result").innerHTML = "Wrong!";
+  if(button.value === correctAnswers[currentQuestionIndex]) {
+    score++;
   }
-  for(let i = 0; i < 4; i++) {
-    document.getElementById(`choice${i + 1}`).disabled = true;
-  }
+  currentQuestionIndex++;
+  displayQuestion();
 }
 
 function displayQuestion () {
-  document.getElementById("question").innerHTML = question;
-  for(let i = 0; i < 4; i++) {
+  if(currentQuestionIndex < questions.length) {
+    document.getElementById("question").innerHTML = questions[currentQuestionIndex];
+  for(let i = 0; i < choicesArray[currentQuestionIndex].length; i++) {
     const btn = document.getElementById(`choice${i+1}`);
-    btn.innerHTML = choices[i];
-    btn.value = choices[i];
+    btn.innerHTML = choicesArray[currentQuestionIndex][i];
+    btn.value = choicesArray[currentQuestionIndex][i];
+  }
+  } else {
+    let buttons = document.querySelectorAll("#choices button");
+    buttons.forEach(button => button.disabled = true);
+    let result = document.getElementById("result");
+    let scoreElement = document.createElement("p");
+    result.appendChild(scoreElement).innerHTML = `You scored ${score} out of ${questions.length}!`;
+
+    document.getElementById("question").innerHTML = "";
+    document.getElementById("choices").innerHTML = "";
   }
 }
 
