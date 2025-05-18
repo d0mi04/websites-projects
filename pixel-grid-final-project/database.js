8c5f85dacbf6155e2c8983f664408e39698002ed
Dominika Windows <d0mi04@windows.com>
2025-05-19 00:12:31 +0200

    pixel-grid, part 1

---
+++ pixel-grid-final-project/database.js
@@ -0,0 +1,39 @@
+const Database = require("better-sqlite3");
+const db = new Database("/tmp/grid.db");
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS grid (
+        x INT,
+        y INT,
+        color TEXT
+    )
+`);
+
+const rowCount = db.prepare(`
+  SELECT COUNT(*) AS count from grid
+`).get().count;
+
+// creating grid for 400 grid objects
+if(rowCount === 0) {
+  const grid = [];
+  for (let x = 0; x < 20; x++) {
+    for(let y = 0; y < 20; y++) {
+      grid.push({x, y, color: "white"});
+    }
+  }
+
+  const insertStatement = db.prepare(`
+    INSERT INTO grid (x, y, color)
+    VALUES (?, ?, ?)
+  `);
+
+  const insertTransaction = db.transaction((rows) => {
+    for(const row of rows) {
+      insertStatement.run(row.x, row.y, row.color);
+    }
+  });
+
+  insertTransaction(grid);
+}
+
+module.exports = db;
